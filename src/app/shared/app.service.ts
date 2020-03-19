7d4d2ddff8033d6cfb78e282bba8bec185ff1bd5
Nishanth <paka.nishanth@accionlabs.com>
2020-03-19 13:26:10 +0530

    Added new service file.

---
+++ src/app/shared/app.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class FileUploadService {
+    /**
+     *
+     */
+    constructor(private http: HttpClient) {
+        
+    }
+
+    uploadFile(user: UserModel) : Observable<any>{
+        return this.http.post<JSON>('http://localhost:58303/api/uploadattachment/', user);
+    }
+}
+
+export class UserModel{
+    name:string;
+    email: string;
+    fileName:string;
+    Attachment: File;
+  }
